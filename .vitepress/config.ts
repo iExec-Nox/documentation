e19eae3701c331d54cf8f7d48d5f3bf2ae1a869b
Le-Caignec <robinlecaignec@yahoo.fr>
2026-03-25 09:38:29 +0100

    chore: add GTM correctly

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -37,6 +37,16 @@ export default withMermaid(
     },
 
     head: [
+      // Google Tag Manager
+      [
+        'script',
+        {},
+        `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://metrics.iex.ec/bs7fgjd8lvy4l31.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-P7KSD4T');`,
+      ],
       ['link', { rel: 'icon', href: '/nox-protocol/Logo-RLC-Yellow.png' }],
       [
         'link',
